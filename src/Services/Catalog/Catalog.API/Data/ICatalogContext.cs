1aac6a5af550105fdb4666ec5476c9ba82019db2
Komanduri, Vishnu <vishnutejak@gmail.com>
2021-08-27 18:30:10 +0530

    CatalogAPI Context

---
+++ src/Services/Catalog/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using Catalog.API.Entities;
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+    }
+}
